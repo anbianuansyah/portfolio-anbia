4e6489411d2cc70c4086106ef55ac4468b8bb9b9
anbia nuansyah <anbianuansyah@gmail.com>
2023-09-11 21:05:43 +0700

    portfolio anbia

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,19 @@ module.exports = {
         birusaya: '#38bdf8',
         gelap: '#0f172a',
         pucat: '#64748b',
-      }
+      },
+      animation: {
+        slide: "slide 2.5s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateY(100%)", opacity: 0.1 },
+          "15%": { transform: "translateY(0)", opacity: 1 },
+          "30%": { transform: "translateY(0)", opacity: 1 },
+          "45%": { transform: "translateY(-100%)", opacity: 1 },
+          "100%": { transform: "translateY(-100%)", opacity: 0.1 },
+        },
+      },
     },
     screens: {
       'sm': '640px',
